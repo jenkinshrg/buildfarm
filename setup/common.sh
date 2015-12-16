c0d18704f95b8f35ff28ca6af503c928e38da79b
jenkinshrg <jenkinshrg@gmail.com>
2015-12-16 16:30:42 +0900

    modify script

--- setup/common.sh
+++ setup/common.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-sudo sed -i -e 's/jp.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
-sudo sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
-
-sudo sed -i -e 's/archive.ubuntu.com/ftp.tsukuba.wide.ad.jp\/Linux/g' /etc/apt/sources.list
-sudo sed -i -e 's/security.ubuntu.com/ftp.tsukuba.wide.ad.jp\/Linux/g' /etc/apt/sources.list
+sudo sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://ftp.jaist.ac.jp/pub/Linux/ubuntu@g" /etc/apt/sources.list
 
 sudo sed -i -e 's/http.debian.net/ftp.jp.debian.org/g' /etc/apt/sources.list
 
 sudo apt-get update
-#sudo apt-get -y upgrade
-#sudo apt-get -y dist-upgrade
-
-sudo apt-get -y install language-pack-ja
-sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
-#source /etc/default/locale
 
 sudo sh -c 'echo Asia/Tokyo > /etc/timezone'
 sudo dpkg-reconfigure --frontend noninteractive tzdata
-
-sudo sed -i -e "s/pc105/jp106/g" /etc/default/keyboard
-sudo sed -i -e "s/us/jp/g" /etc/default/keyboard
-#source /etc/default/keyboard
 
 sudo apt-get -y install software-properties-common
 sudo apt-get -y install python-software-properties
