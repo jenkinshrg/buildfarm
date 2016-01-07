796d0c95cbd84e111eed2da23cbf0d498e428ca7
jenkinshrg <jenkinshrg@gmail.com>
2016-01-07 14:44:05 +0900

    modify script

--- setup/server.sh
+++ setup/server.sh
@@ -33,7 +33,7 @@ pre {
 }
 EOL
 
-sudo sh -c "echo '30 * * * * vagrant cd /home/vagrant && rm -fr jenkinshrg.github.io && git clone https://github.com/jenkinshrg/jenkinshrg.github.io.git && cd jenkinshrg.github.io && bash -xe .jenkins.sh' >> /etc/crontab"
+sudo sh -c "echo '59 * * * * vagrant cd /home/vagrant && rm -fr jenkinshrg.github.io && git clone https://github.com/jenkinshrg/jenkinshrg.github.io.git && cd jenkinshrg.github.io && bash -xe .jenkins.sh' >> /etc/crontab"
 sudo service cron restart
 
 cat << EOL | python
