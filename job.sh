5b217c272c8de9500e8d3803360b279c642816f4
jenkinshrg <jenkinshrg@gmail.com>
2016-02-08 15:04:12 +0900

    modify script

--- job.sh
+++ job.sh
@@ -1,12 +1,13 @@
-./scripts/cli/createjob.sh drcutil https://github.com/jenkinshrg/drcutil.git drcutil jenkins slave scm
-./scripts/cli/createjob.sh", args: "drcutil-build-32 https://github.com/jenkinshrg/drcutil.git drcutil jenkins debian-wheezy-i386 upstream build
-./scripts/cli/createjob.sh", args: "drcutil-build-64 https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64 upstream build
-./scripts/cli/createjob.sh", args: "drcutil-test https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64 upstream test
-./scripts/cli/createjob.sh", args: "drcutil-analysis https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64 upstream analysis
-./scripts/cli/createjob.sh", args: "drcutil-inspection https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64 upstream inspection
-./scripts/cli/createjob.sh", args: "drcutil-task-balancebeam https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64-desktop periodic task balancebeam
-./scripts/cli/createjob.sh", args: "drcutil-task-terrain https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64-desktop periodic task terrain
-./scripts/cli/createjob.sh", args: "drcutil-task-valve https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64-desktop periodic task valve
-./scripts/cli/createjob.sh", args: "drcutil-task-walk https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64-desktop periodic task walk
-./scripts/cli/createjob.sh", args: "drcutil-task-wall https://github.com/jenkinshrg/drcutil.git drcutil jenkins ubuntu-trusty-amd64-desktop periodic task wall
-./scripts/cli/createjob.sh", args: "report https://github.com/jenkinshrg/jenkinshrg.github.io.git jenkinshrg.github.io master slave
+#                      jobname                  repository                                             directory            branch  node                        trigger  script params
+./scripts/createjob.sh drcutil                  https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins slave                       scm
+./scripts/createjob.sh drcutil-build-32         https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins debian-wheezy-i386          upstream build
+./scripts/createjob.sh drcutil-build-64         https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64         upstream build
+./scripts/createjob.sh drcutil-test             https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64         upstream test
+./scripts/createjob.sh drcutil-analysis         https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64         upstream analysis
+./scripts/createjob.sh drcutil-inspection       https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64         upstream inspection
+./scripts/createjob.sh drcutil-task-balancebeam https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64-desktop periodic task balancebeam
+./scripts/createjob.sh drcutil-task-terrain     https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64-desktop periodic task terrain
+./scripts/createjob.sh drcutil-task-valve       https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64-desktop periodic task valve
+./scripts/createjob.sh drcutil-task-walk        https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64-desktop periodic task walk
+./scripts/createjob.sh drcutil-task-wall        https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins ubuntu-trusty-amd64-desktop periodic task wall
+./scripts/createjob.sh report                   https://github.com/jenkinshrg/jenkinshrg.github.io.git jenkinshrg.github.io master  slave
