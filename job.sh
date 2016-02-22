ad072171b5f90739a33ef4f60d009fd8da83dfc2
jenkinshrg <jenkinshrg@gmail.com>
2016-02-22 13:59:52 +0900

    modify script

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
-#                      jobname                  repository                                             directory            branch  node           os     distro arch  trigger  func  test        url
-./scripts/createjob.sh drcutil                  https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.15  ubuntu trusty amd64 scm      all   all         http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-build-32         https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.15  debian wheezy i386  upstream build all         http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-build-64         https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.15  ubuntu trusty amd64 upstream build all         http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-task-balancebeam https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.169 ubuntu trusty amd64 periodic task  balancebeam http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-task-terrain     https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.169 ubuntu trusty amd64 periodic task  terrain     http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-task-valve       https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.169 ubuntu trusty amd64 periodic task  valve       http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh drcutil-task-wall        https://github.com/jenkinshrg/drcutil.git              drcutil              jenkins 150.29.145.169 ubuntu trusty amd64 periodic task  wall        http://jenkinshrg.a01.aist.go.jp
-./scripts/createjob.sh report                   https://github.com/jenkinshrg/jenkinshrg.github.io.git jenkinshrg.github.io master  slave          ubuntu trusty amd64 none     all   all         http://jenkinshrg.a01.aist.go.jp
+#                      jobname                  node           os     distro arch  trigger  func           script_args
+./scripts/createjob.sh drcutil                  150.29.145.15  none   none   none  scm      .jenkinshrg.sh
+./scripts/createjob.sh drcutil-build-32         150.29.145.15  debian wheezy i386  upstream .jenkinshrg.sh build
+./scripts/createjob.sh drcutil-build-64         150.29.145.15  ubuntu trusty amd64 upstream .jenkinshrg.sh build
+./scripts/createjob.sh drcutil-task-balancebeam 150.29.145.169 none   none   none  periodic .jenkinshrg.sh task balancebeam
+./scripts/createjob.sh drcutil-task-terrain     150.29.145.169 none   none   none  periodic .jenkinshrg.sh task terrain
+./scripts/createjob.sh drcutil-task-valve       150.29.145.169 none   none   none  periodic .jenkinshrg.sh task valve
+./scripts/createjob.sh drcutil-task-wall        150.29.145.169 none   none   none  periodic .jenkinshrg.sh task wall
+./scripts/createjob.sh report                   150.29.145.15  none   none   none  none     .jenkinshrg.sh
