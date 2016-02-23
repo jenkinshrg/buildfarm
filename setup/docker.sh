4de30a1d2b4391ae67af8af3d677d1fb44b7642c
jenkinshrg <jenkinshrg@gmail.com>
2016-02-23 14:01:29 +0900

    modify script

--- setup/docker.sh
+++ setup/docker.sh
@@ -2,5 +2,8 @@
 
 wget -qO- https://get.docker.com/ | sh
 
+sudo sh -c 'echo "DOCKER_OPTS=\"--dns=150.29.246.19 --dns=150.29.254.121\"" >> /etc/default/docker'
+sudo service docker restart
+
 sudo usermod -aG docker $USER
 #sudo su - $USER
