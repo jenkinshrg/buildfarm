8e2b98975551c7dbe66ad9f5490442cec4db6d6e
jenkinshrg <jenkinshrg@gmail.com>
2016-02-24 15:40:16 +0900

    modify script

--- setup/common.sh
+++ setup/common.sh
@@ -7,10 +7,6 @@ sudo apt-get -y install daemon
 sudo apt-get -y install openjdk-7-jdk
 
 sudo apt-get -y install git
-git config --global user.email "jenkinshrg@gmail.com"
-git config --global user.name "jenkinshrg"
-git config --global credential.helper store
-git config --global http.sslVerify false
 
 sudo apt-get -y install unattended-upgrades
 #sudo dpkg-reconfigure -plow unattended-upgrades
