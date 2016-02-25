c3441bd7d64b6d7b4c52cf56c47f0cf3ea1027f0
jenkinshrg <jenkinshrg@gmail.com>
2016-02-25 09:42:43 +0900

    modify script

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -4,5 +4,4 @@ sudo sh -c 'echo Asia/Tokyo > /etc/timezone'
 sudo dpkg-reconfigure --frontend noninteractive tzdata
 
 sudo sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://ftp.jaist.ac.jp/pub/Linux/ubuntu@g" /etc/apt/sources.list
-sudo sed -i -e 's/http.debian.net/ftp.jp.debian.org/g' /etc/apt/sources.list
 sudo apt-get update
