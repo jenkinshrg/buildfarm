dc3bdd5a5cd94b59f97f50bd6ed4c267d22bae19
jenkinshrg <jenkinshrg@gmail.com>
2018-06-08 20:16:14 +0900

    pass username and password to jenkins slave

--- setup/slave_desktop.sh
+++ setup/slave_desktop.sh
@@ -10,11 +10,16 @@ URL=${2:-http://jenkinshrg.a01.aist.go.jp}
 
 wget -q -O $HOME/slave.jar $URL/jnlpJars/slave.jar
 
+echo -n "user name to access jenkins master: "
+read user
+echo -n "password: "
+read passwd
+
 mkdir -p $HOME/.config/autostart
 cat << EOL | tee $HOME/.config/autostart/jenkins-slave.desktop
 [Desktop Entry]
 Type=Application
-Exec=java -jar $HOME/slave.jar -jnlpUrl $URL/computer/$NAME/slave-agent.jnlp
+Exec=java -jar $HOME/slave.jar -jnlpUrl $URL/computer/$NAME/slave-agent.jnlp -jnlpCredentials $user:$passwd
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
@@ -22,6 +27,6 @@ Name=Jenkins Slave
 Comment=Start Jenkins Slave For Desktop Application
 EOL
 
-java -jar $HOME/slave.jar -jnlpUrl $URL/computer/$NAME/slave-agent.jnlp &
+java -jar $HOME/slave.jar -jnlpUrl $URL/computer/$NAME/slave-agent.jnlp -jnlpCredentials $user:$passwd &
 
 sudo sh -c 'echo "'$USER' ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers'
